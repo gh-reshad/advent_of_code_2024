46cde2318c98b0fb9441856777e5ebc5220e7817
gh-reshad <reshad.ghazal@yahoo.com>
2024-12-03 21:22:43 -0500

    part two added!

--- problem_3_2024.py
+++ problem_3_2024.py
@@ -1,23 +1,29 @@
 import re
 
 with open(r'input\inp_3_2024.txt', 'r') as file:
-    lines = file.readlines()
+    lines = file.read()
+
+def enabled():
+    new_line = ""
+    for l in lines.split('do()'):
+        new_line += l.split('don\'t()')[0]
+    sum_of_multiplications(new_line)
 
 
-def sum_of_multiplications():
+def sum_of_multiplications(line):
     multi = 0
-    for line in lines:
-        pattern = r'mul\((\d+),(\d+)\)'
+    pattern = r'mul\((\d+),(\d+)\)'
 
-        matches = re.findall(pattern, line)
+    matches = re.findall(pattern, line)
 
-        for match in matches:
-            print(match)
-            x,y = int(match[0]), int(match[1])
-            multi += x * y
+    for match in matches:
+        #print(match)
+        x,y = int(match[0]), int(match[1])
+        multi += x * y
 
     print(multi)
 
 
 if __name__ == "__main__":
-    sum_of_multiplications()
+    enabled()
+    
